ea5c0314870d78dc688b6d88a19c6a4990a323cc
ChimexXy <moadzahnoun37@gmail.com>
2025-09-12 15:58:18 +0100

    chimex

--- cp00/ex01/phonebook.cpp
+++ cp00/ex01/phonebook.cpp
@@ -1,7 +1,47 @@
 #include "phonebook.hpp"
 
+void add(phonebook x)
+{
+    std::cout << "enter the first.name" << std::endl;
+    getline(std::cin, x.first_name);
+    std::cout << x.first_name << std::endl;
+
+    std::cout << "enter the last name" << std::endl;
+    getline(std::cin, x.last_name);
+    std::cout << x.last_name << std::endl;
+
+    std::cout << "enter the nick name" << std::endl;
+    getline(std::cin, x.nick_name);
+    std::cout << x.nick_name << std::endl;
+
+    std::cout << "enter the phone number" << std::endl;
+    getline(std::cin, x.phone_number);
+    std::cout << x.phone_number << std::endl;
+
+    std::cout << "enter your darkest_secret" << std::endl;
+    getline(std::cin, x.darkest_secret);
+    std::cout << x.darkest_secret << std::endl;
+}
+
 int main()
 {
-    int i = 0;
-    
+    phonebook x;
+    x.count = 0;
+    while(1)
+    {
+        
+        std::cout << "enter the command" << std::endl;
+        getline(std::cin, x.command);
+        if(x.command == "ADD")
+            add(x);
+        else if(x.command == "SEARCH")
+            std::cout << "search the contact" << std::endl;
+        else if(x.command == "EXIT")
+        {
+            std::cout << "exit the programme" << std::endl;
+            exit(1);
+        }
+        else
+            std::cout << "Invalid command" << std::endl;
+    }
 }
--- cp00/ex01/phonebook.hpp
+++ cp00/ex01/phonebook.hpp
@@ -2,5 +2,17 @@
 #define PHONEBOOk_HPP
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
+class phonebook{
+public:
+    int count;
+    std::string command;
+    std::string first_name;
+    std::string last_name;
+    std::string nick_name;
+    std::string phone_number;
+    std::string darkest_secret;
+};
 #endif
