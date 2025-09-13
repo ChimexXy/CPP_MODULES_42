8ae0888402bd11f4b4888789f727bd24f91d0c3e
ChimexXy <moadzahnoun37@gmail.com>
2025-09-13 13:12:26 +0100

    chimex

--- test.cpp
+++ test.cpp
@@ -23,43 +23,58 @@
 // // }
 
 
+// #include <iostream>
+// #include <limits>
+
+// int check(char *num)
+// {
+// 	int i = 0;
+// 	if(!num)
+// 		return (0);
+// 	for (i = 0; num[i]; i++)
+// 	{
+// 		if(!(num[i] >= '0' && num[i] <= '9'))
+// 			return (1);
+// 	}
+// 	return (0);
+// }
+
+// using namespace std;
+
+// int main()
+// {
+// 	int num;
+
+// 	while (1) {
+// 		cout << "Enter a valid number: herre " << endl;
+// 		cin >> num;
+
+// 		if (!cin.fail())
+// 			break; // input successful, exit loop
+
+// 		// input failed
+// 		cout << "Invalid input. Try again.\n";
+
+// 		cin.clear(); // fix error state
+// 		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // remove garbage
+// 	}
+
+// 	cout << "You entered: " << num << endl;
+
+// 	return 0;
+// }
+
+
 #include <iostream>
-#include <limits>
-
-int check(char *num)
-{
-	int i = 0;
-	if(!num)
-		return (0);
-	for (i = 0; num[i]; i++)
-	{
-		if(!(num[i] >= '0' && num[i] <= '9'))
-			return (1);
-	}
-	return (0);
-}
-
+#include <string>
 using namespace std;
 
-int main()
-{
-	int num;
-
-	while (1) {
-		cout << "Enter a valid number: herre " << endl;
-		cin >> num;
-
-		if (!cin.fail())
-			break; // input successful, exit loop
-
-		// input failed
-		cout << "Invalid input. Try again.\n";
-
-		cin.clear(); // fix error state
-		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // remove garbage
-	}
-
-	cout << "You entered: " << num << endl;
-
-	return 0;
+int main() {
+    string line;
+    while(1)
+    { 
+        cout << "ktb chi 7aja: ";
+        getline(cin, line); // hna kay9ra lline kamla
+        cout << "khtarti: " << line << endl;
+    }
 }
