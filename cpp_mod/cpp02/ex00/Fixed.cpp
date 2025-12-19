9fb9f25d6d412d2b98c576d9beb1cabc30fd1b5d
ChimexXy <moadzahnoun37@gmail.com>
2025-12-19 21:13:04 +0100

    chimex

--- cpp_mod/cpp02/ex00/Fixed.cpp
+++ cpp_mod/cpp02/ex00/Fixed.cpp
@@ -1,19 +1,16 @@
 #include "Fixed.hpp"
 
-// Default constructor
 Fixed::Fixed() : _value(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
-// Copy constructor
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
     this->_value = other.getRawBits();
 }
 
-// Copy assignment operator
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
@@ -22,20 +19,16 @@ Fixed &Fixed::operator=(const Fixed &other)
     return *this;
 }
 
-// Destructor
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
 }
 
-// Getter
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
     return this->_value;
 }
 
-// Setter
 void Fixed::setRawBits(int const raw)
 {
     this->_value = raw;
