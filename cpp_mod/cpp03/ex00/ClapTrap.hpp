378c0afedf2eec427946e71524716f99d97ef5f5
ChimexXy <moadzahnoun37@gmail.com>
2026-01-01 13:26:05 +0100

    chimex

--- cpp_mod/cpp03/ex00/ClapTrap.hpp
+++ cpp_mod/cpp03/ex00/ClapTrap.hpp
@@ -3,4 +3,22 @@
 
 #include <iostream>
 
+class ClapTrap{
+private:
+	std::string _name;
+	int _hitPoints;
+	int _energyPoints;
+	int _attackDamage;
+
+
+ 
+public:
+
+
+void attack(const std::string& target);
+void takeDamage(unsigned int amount);
+void beRepaired(unsigned int amount);
+
+};
+
 #endif
