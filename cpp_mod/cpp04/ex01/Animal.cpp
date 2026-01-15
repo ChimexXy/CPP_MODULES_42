748d8febc707206d8facd72e396579b6df623bbb
ChimexXy <moadzahnoun37@gmail.com>
2026-01-15 21:45:05 +0100

    chime

---
+++ cpp_mod/cpp04/ex01/Animal.cpp
@@ -0,0 +1,34 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    type = "Animal";
+    std::cout << "Animal constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other)
+{
+    *this = other;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+    if (this != &other)
+        type = other.type;
+    return *this;
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal destructor called" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+    return type;
+}
+
+void Animal::makeSound() const
+{
+    std::cout << "Animal makes a sound" << std::endl;
+}
