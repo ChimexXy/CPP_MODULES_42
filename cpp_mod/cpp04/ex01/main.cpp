89c50cd2baccb0b4f40aaffffcb52fd30cae04ee
ChimexXy <moadzahnoun37@gmail.com>
2026-01-17 18:24:21 +0100

    chimex

--- cpp_mod/cpp04/ex01/main.cpp
+++ cpp_mod/cpp04/ex01/main.cpp
@@ -1,19 +1,33 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
-int main()
-{
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-return 0;
+int main() {
+    std::cout << "=== Testing WrongAnimal OCF ===" << std::endl;
+    
+    // Default constructor
+    WrongAnimal wa1;
+    
+    // Copy constructor
+    WrongAnimal wa2(wa1);
+    
+    // Assignment operator
+    WrongAnimal wa3;
+    wa3 = wa1;
+    
+    std::cout << "\n=== Testing WrongCat OCF ===" << std::endl;
+    
+    // Default constructor
+    WrongCat wc1;
+    
+    // Copy constructor
+    WrongCat wc2(wc1);
+    
+    // Assignment operator
+    WrongCat wc3;
+    wc3 = wc1;
+    
+    return 0;
 }
