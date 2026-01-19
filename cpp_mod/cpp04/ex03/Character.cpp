5342759e49c947bf791f7b952a3d778e282892b9
chimexXy <moadzahnoun37@gmail.com>
2026-01-19 23:12:33 +0100

    chimex

--- cpp_mod/cpp04/ex03/Character.cpp
+++ cpp_mod/cpp04/ex03/Character.cpp
@@ -48,7 +48,7 @@ Character& Character::operator=(const Character& other)
     std::cout << "Character assignment operator called" << std::endl;
     if (this != &other) {
         this->Name = other.Name;
-        
+
         // Delete old inventory
         for (int i = 0; i < 4; i++) {
             if (this->Inventory[i]) {
@@ -98,7 +98,6 @@ void Character::equip(AMateria* m)
         std::cout << "Cannot equip NULL materia" << std::endl;
         return;
     }
-    
     // Find first empty slot
     for (int i = 0; i < 4; i++) {
         if (this->Inventory[i] == NULL) {
@@ -107,7 +106,6 @@ void Character::equip(AMateria* m)
             return;
         }
     }
-    
     // Inventory full - drop to floor
     std::cout << Name << "'s inventory is full! Materia dropped to floor." << std::endl;
     if (FloorCount < FloorSize)
