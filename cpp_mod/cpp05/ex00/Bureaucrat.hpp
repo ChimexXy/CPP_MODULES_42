bc693791fd94fde93f68f2c541b91e1ddc383669
ChimexXy <moadzahnoun37@gmail.com>
2026-01-26 15:19:50 +0100

    chimex

---
+++ cpp_mod/cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,17 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat{
+private:
+	const std::string _name;
+	int _grade;
+	static const int _highset = 1;
+	static const int _lowset = 150;
+
+public:
+
+};
+
+#endif
