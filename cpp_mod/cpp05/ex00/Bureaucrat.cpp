4887ea403d578249fa774a9b8d53c154233c6f96
ChimexXy <moadzahnoun37@gmail.com>
2026-01-27 16:08:42 +0100

    chimex

--- cpp_mod/cpp05/ex00/Bureaucrat.cpp
+++ cpp_mod/cpp05/ex00/Bureaucrat.cpp
@@ -1,1 +1,42 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	_name = "moad";
+	_grade = 42;
+	std::cout << "Default constractor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& org)
+{
+	std::cout << "copy constractor called" << std::endl;
+	this->_name = org._name;
+	this->_grade = org._grade;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& org)
+{
+	std::cout << "assignment operator called" << std::endl;
+	if (this != &org)
+	_grade = org._grade;
+return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "destractor called for " << _name << std::endl;
+}
+
+// Bureaucrat::Bureaucrat(const std::string& name, int grade)
+// {
+		// this->_name = org._name;
+
+// 	    std::cout << "Bureaucrat constructor called for " << _name << std::endl;
+    
+//     if (grade < HIGHEST_GRADE)
+//         throw GradeTooHighException();
+//     if (grade > LOWEST_GRADE)
+//         throw GradeTooLowException();
+    
+//     _grade = grade;
+// }
--- cpp_mod/cpp05/ex00/Bureaucrat.hpp
+++ cpp_mod/cpp05/ex00/Bureaucrat.hpp
@@ -1,11 +1,12 @@
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <exception>
 #include <iostream>
 
 class Bureaucrat{
 private:
-	const std::string _name;
+	std::string _name;
 	int _grade;
 	static const int _highset = 1;
 	static const int _lowset = 150;
@@ -17,6 +18,17 @@ public:
 	~Bureaucrat();
 
 	Bureaucrat(const std::string& name, int grade);
+	
+	class GradeTooHighException : public std::exception{
+	public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception{
+	public:
+		virtual const char* what() const throw();
+	};
+
+
 };
 
 #endif
